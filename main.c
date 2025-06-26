83de70d2ac02979ee5af3a01abf16781f87e38f0
RyanYang99 <diddbstjd0220@naver.com>
2025-06-26 14:38:28 +0900

    Project Setup

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/// <summary>
+///	2025 여름 심화학기 프로젝트
+/// 6/30까지 - PPT작성 [팀이름, 팀내에서의 역할, 주제와 게임의 이름, 어떤 게임인지 설명(개요)]
+/// </summary>
+
+void main()
+{
+
+}
