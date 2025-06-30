c66a9de126fad2687f5436dd01c8acef7287afec
KnockKnockP <hansu012345@gmail.com>
2025-07-01 07:04:41 +0900

    함수로 정리, y축 로직 수정

--- map.c
+++ map.c
@@ -97,25 +97,27 @@ void destroy_map(void)
     free(map.ppBlocks);
 }
 
-static COORD render_block(const POINT map_position, const COORD console_position, const bool ltr)
+static COORD render_block(const POINT map_position, const COORD console_position, const bool ltr, const bool utd)
 {
     bool exit = false;
     COORD size = { 0 };
 
     for (int ty = 0; ty < TEXTURE_SIZE; ++ty)
     {
-        for (int tx = ltr ? 0 : TEXTURE_SIZE; ltr ? (tx < TEXTURE_SIZE) : (tx > 0); ltr ? ++tx : --tx)
+        for (int tx = 0; tx < TEXTURE_SIZE; ++tx)
         {
-            const COORD position = { console_position.X + (SHORT)(ltr ? tx : -(TEXTURE_SIZE - tx)), console_position.Y + (SHORT)ty };
+            const COORD position = { console_position.X + (SHORT)(ltr ? tx : -tx), console_position.Y + (SHORT)(utd ? ty: -ty) };
 
-            exit = (position.X < 0 || position.X >= console.size.X) && position.Y >= console.size.Y;
-            if (position.X < 0 || position.X >= console.size.X || position.Y >= console.size.Y)
+            exit = (position.X < 0 || position.X >= console.size.X) && (position.Y < 0 || position.Y >= console.size.Y);
+            if (position.X < 0 || position.X >= console.size.X || position.Y < 0 || position.Y >= console.size.Y)
                 break;
 
-            size.X = (SHORT)(ltr ? tx + 1 : TEXTURE_SIZE - tx + 1);
+            size.X = (SHORT)tx + 1;
             size.Y = (SHORT)ty + 1;
 
-            print_color_tchar(pBlock_textures[map.ppBlocks[map_position.y][map_position.x] - 1][ty][ltr ? tx : (tx - 1)], position);
+            print_color_tchar(pBlock_textures[map.ppBlocks[map_position.y][map_position.x] - 1]
+                                             [utd ? ty : (TEXTURE_SIZE - ty - 1)][ltr ? tx : (TEXTURE_SIZE - tx - 1)],
+                              position);
         }
 
         if (exit)
@@ -125,66 +127,47 @@ static COORD render_block(const POINT map_position, const COORD console_position
     return size;
 }
 
+static COORD render_aft_or_forward(const COORD console_position_half, COORD console_position, const bool forward)
+{
+    COORD texture_size = { 0 };
+
+    for (int x = forward ? player.x : (player.x - 1); forward ? (x < map.size.x) : x >= 0; forward ? ++x : --x)
+    {
+        for (int y = player.y; y < map.size.y; ++y)
+        {
+            texture_size = render_block((POINT) { x, y }, console_position, forward, true);
+            console_position.Y += texture_size.Y;
+
+            if (console_position.Y >= console.size.Y)
+                break;
+        }
+
+        console_position.Y = console_position_half.Y - 1;
+        for (int y = player.y - 1; y >= 0; --y)
+        {
+            texture_size = render_block((POINT) { x, y }, console_position, forward, false);
+            console_position.Y -= texture_size.Y;
+
+            if (console_position.Y < 0)
+                break;
+        }
+
+        console_position.X += forward ? texture_size.X : -texture_size.X;
+        console_position.Y = console_position_half.Y;
+        if ((forward && console_position.X >= console.size.X) || (!forward && console_position.X < 0))
+            break;
+    }
+
+    return console_position;
+}
+
 void render_map(void)
 {
     const COORD console_position_half = { console.size.X / 2, console.size.Y / 2 };
-    COORD console_position = console_position_half, texture_size = { 0 };
 
-    for (int x = player.x; x < map.size.x; ++x)
-    {
-        for (int y = player.y; y < map.size.y; ++y)
-        {
-            texture_size = render_block((POINT){ x, y }, console_position, true);
-            console_position.Y += texture_size.Y;
-
-            if (console_position.Y >= console.size.Y)
-                break;
-        }
-
-        console_position.Y = console_position_half.Y - texture_size.Y;
-        for (int y = player.y - 1; y >= 0; --y)
-        {
-            texture_size = render_block((POINT){ x, y }, console_position, true);
-            console_position.Y -= texture_size.Y;
-            
-            if (console_position.Y < 0)
-                break;
-        }
-
-        console_position.X += texture_size.X;
-        console_position.Y = console_position_half.Y;
-        if (console_position.X >= console.size.X)
-            break;
-    }
-
+    COORD console_position = render_aft_or_forward(console_position_half, console_position_half, true);
     console_position.X = console_position_half.X - 1;
-
-    for (int x = player.x - 1; x >= 0; --x)
-    {
-        for (int y = player.y; y < map.size.y; ++y)
-        {
-            texture_size = render_block((POINT) { x, y }, console_position, false);
-            console_position.Y += texture_size.Y;
-
-            if (console_position.Y >= console.size.Y)
-                break;
-        }
-
-        console_position.Y = console_position_half.Y - texture_size.Y;
-        for (int y = player.y - 1; y >= 0; --y)
-        {
-            texture_size = render_block((POINT) { x, y }, console_position, false);
-            console_position.Y -= texture_size.Y;
-
-            if (console_position.Y < 0)
-                break;
-        }
-
-        console_position.X -= texture_size.X;
-        console_position.Y = console_position_half.Y;
-        if (console_position.X < 0)
-            break;
-    }
+    render_aft_or_forward(console_position_half, console_position, false);
 }
 
 #if _DEBUG
